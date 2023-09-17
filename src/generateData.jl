7ef2d73d33a3c5b6ee8058145d30620d46c6d105
korilium <ignace.decocq@hotmail.com>
2023-09-17 23:54:40 +0200

    data generation

---
+++ src/generateData.jl
@@ -0,0 +1,44 @@
+using CSV, DataFrames, Statistics, StatsBase, Random
+
+
+#data 
+df = CSV.File("data/names.csv") |> DataFrame
+
+describe(df)
+
+
+length = size(df)[1] 
+
+#parameters 
+n = 30 
+seedValue = 666
+Random.seed!(seedValue)
+workingAge = 18 
+legalRetirmentAge = 67 
+
+randomIndices = sample(1:length, n ,replace=false)
+
+Beneficiary = df[randomIndices,:]
+
+
+Age = rand(workingAge:legalRetirmentAge, n)
+
+Beneficiary[:, "age"] = Age
+
+pst = []
+for age in Age 
+    proportion = rand()
+    chance1= rand()
+    chance2 = rand()
+    if (proportion > chance1) & (proportion > chance2) 
+        Pst = (age - workingAge) * proportion
+    else 
+        Pst = age - workingAge
+    end 
+    Pst = round(Pst, digits = 2)
+    push!(pst, Pst)
+end 
+
+Beneficiary[:, "pst"] = pst
+
+Beneficiary[:, "tst"] = .- pst .+ legalRetirmentAge .- workingAge
